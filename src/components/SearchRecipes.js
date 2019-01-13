1ff520a737e25932b256e795039d6e78129e0ea5
Anto Rodrigo <infaroshni456@gmail.com>
2019-01-13 23:51:36 +0530

    Fetching from the API

--- src/components/SearchRecipes.js
+++ src/components/SearchRecipes.js
@@ -14,7 +14,12 @@ class SearchRecipes extends Component {
     search() {
         let {ingredients, dish} = this.state;
         const url = `http://www.recipepuppy.com/api?i=${ingredients}&q=${dish}`;
-        console.log('state', this.state, 'url', url);
+
+        // Fetch method to access API
+        fetch(url, {
+            method: 'GET'
+        }).then(response => response.json())
+          .then(json => console.log('Response: ', json))
     }
 
     render() {
